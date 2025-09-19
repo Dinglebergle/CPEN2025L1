12e7503ec834664c03638784d31382c663a3a27e
Hubert Theodore <hubertotheo@gmail.com>
2025-09-19 15:34:06 -0700

    fix tb_task5 (all tests pass now)

--- task5/tb_task5.sv
+++ task5/tb_task5.sv
@@ -270,21 +270,27 @@ module tb_task5();
 
         // Testing dealer win
         $display("Testing dealer win-------------------");
-        // Get to DC2 state and load player card 1 (Ace), dealer card 1 (Ace), player card 2 (Ace)
+        // Get to DC1 state and load player card 1 (Ace)
         slow_clock_cycle();
+        // Deal 8 card to dealer card 1
+        fast_clock_cycle();
+        fast_clock_cycle();
+        fast_clock_cycle();
+        fast_clock_cycle();
+        fast_clock_cycle();
+        fast_clock_cycle();
+        fast_clock_cycle();
+        // Get to PC2 state and load dealer card 1 (8)
         slow_clock_cycle();
+        // Deal 9 card to player card 2
+        fast_clock_cycle();
+        // Get to DC2 state and load player card 2 (9)
         slow_clock_cycle();
-        // Deal 8 card to dealer card 2
+        // Deal 10 card to dealer card 2
         fast_clock_cycle();
-        fast_clock_cycle();
-        fast_clock_cycle();
-        fast_clock_cycle();
-        fast_clock_cycle();
-        fast_clock_cycle();
-        fast_clock_cycle();
-        // Cycle to declareWinner state
+        // Cycle to declareWinner state and load dealer card 2 (10)
         slow_clock_cycle();
-        check_led({dealer_win_light, player_win_light, dscore, pscore}, {1'b1, 1'b0, 4'b1001, 4'b0010}); // Check dealer_win_light is on, dealer score is 9 and player score is 2
+        check_led({dealer_win_light, player_win_light, dscore, pscore}, {1'b1, 1'b0, 4'b1000, 4'b0000}); // Check dealer_win_light is on, dealer score is 8 and player score is 0
 
         reset();
 
